753b41d6183ddf2887bcc1178b7304dd1b67f56b
random-randoms <daniil.savinov.05@gmail.com>
2023-11-03 14:07:29 +0300

    Adding attemp to read stats from table

--- DataBasePeeker.py
+++ DataBasePeeker.py
@@ -7,12 +7,14 @@ class DataBasePeeker:
         self.conn = psycopg2.connect(database="db_name",
                                      host="dp_host",
                                      user="db_user",
-                                     password="dp_pass",
-                                     port="db_port")
+                                     password="dp_pass")
         self.cursor = self.conn.cursor()
 
     # interface methods
     def peek(self) -> int:
+        self.cursor.execute("SELECT * FROM pg_stat_activity")
+        stats = self.cursor.fetchone()
+        print(stats)
         return 0
 
     def fix(self, option_code):
