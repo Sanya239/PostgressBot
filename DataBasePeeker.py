c9e88a693a87749d012268a086b35aa2868efda6
random-randoms <daniil.savinov.05@gmail.com>
2023-11-03 16:58:43 +0300

    Fixing DataBAsePeeker.__init__ signature

--- DataBasePeeker.py
+++ DataBasePeeker.py
@@ -3,18 +3,28 @@ import psycopg2
 
 class DataBasePeeker:
     # init
-    def __init__(self):
-        self.conn = psycopg2.connect(database="db_name",
-                                     host="dp_host",
-                                     user="db_user",
-                                     password="dp_pass")
-        self.cursor = self.conn.cursor()
+    def __init__(self, database, host, user, password):
+        self.conn = psycopg2.connect(database=database,
+                                     host=host,
+                                     user=user,
+                                     password=password)
+        print("Connection established")
+        self.online = True
 
-    # interface methods
+    def __del__(self):
+        self.conn.close()
+
+# interface methods
     def peek(self) -> int:
-        self.cursor.execute("SELECT * FROM pg_stat_activity")
-        stats = self.cursor.fetchone()
-        print(stats)
+        with self.conn.cursor() as crs:
+            crs.execute("SELECT * FROM public.\"SomeTable\"")
+            f = crs.fetchone()
+            print(f)
+            crs.execute("select current_timestamp - query_start as runtime, datname, usename,"+
+                        " query from pg_stat_activity where query != '<IDLE>' order by 1 desc;")
+            (state) = crs.fetchone()
+            print(state)
+
         return 0
 
     def fix(self, option_code):
