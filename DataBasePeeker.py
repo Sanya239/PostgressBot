d7ce773bc56c7198cf81d42c87512aa001e06b7b
random-randoms <daniil.savinov.05@gmail.com>
2023-11-04 09:15:36 +0300

    Adding stuff

--- DataBasePeeker.py
+++ DataBasePeeker.py
@@ -1,30 +1,105 @@
 import psycopg2
+import datetime
 
 
 class DataBasePeeker:
     # init
     def __init__(self, database, host, user, password):
         self.database = database
-        self.conn = psycopg2.connect(database=database,
-                                     host=host,
-                                     user=user,
-                                     password=password)
+        self.max_query_time = 1
+        self.max_process_time = 60
+        self.longest_process = 0
+        self.long_query_pids = []
+        self.long_process_pids = []
+        self._conn = psycopg2.connect(database=database,
+                                      host=host,
+                                      user=user,
+                                      password=password)
         print("Connection established")
-        self.online = True
 
     def __del__(self):
-        self.conn.close()
+        self._conn.close()
 
 # interface methods
-    def peek(self) -> int:
-        with self.conn.cursor() as crs:
-            crs.execute("SELECT * FROM public.\"SomeTable\"")
-            f = crs.fetchone()
-            print(f)
-            crs.execute("select * from pg_stat_activity")
+    def peek(self):
+        with self._conn.cursor() as crs:
+            crs.execute("select clock_timestamp() - query_start as qruntime," +
+                        " clock_timestamp() - backend_start as bruntime, pid, datname, usename, query" +
+                        " from pg_stat_activity" +
+                        " where query_start is not null" +
+                        " and pid <> pg_backend_pid()" +
+                        " order by 1 desc;")
             stats = crs.fetchall()
-
-        return 0
+            report = ""
+            for stat in stats:
+                #print(stat)
+                self.check_query_running_time(stat)
+                self.check_process_running_time(stat)
+            report += self.query_running_report()
+            report += self.process_running_report()
+            report += self.check_max_process_time()
+            return report
 
     def fix(self, option_code):
-        pass
+        if option_code == 1:
+            return self.term_long_queries()
+        if option_code == 2:
+            return self.term_long_processes()
+        if option_code == 3:
+            return self.checkpoint()
+
+    # internal methods
+    def check_max_process_time(self):
+        return "current max process time is {}\n".format(self.longest_process)
+
+    def check_query_running_time(self, stat):
+        self.long_query_pids = []
+        (qtime, btime, pid, datname, usename, query) = stat
+        process_time = qtime.total_seconds()
+        if process_time > self.max_query_time:
+            #print("LOG: query in process " + str(pid) + " is running for " + str(process_time))
+            self.long_query_pids.append(pid)
+
+    def check_process_running_time(self, stat):
+        self.long_process_pids = []
+        self.longest_process = 0
+        (time, btime, pid, datname, usename, query) = stat
+        process_time = btime.total_seconds()
+        if process_time > self.longest_process:
+            self.longest_process = process_time
+        if process_time > self.max_process_time:
+            #print("LOG: process " + str(pid) + " is running for " + str(process_time))
+            self.long_process_pids.append(pid)
+
+    def query_running_report(self):
+        return "{} process queries exceeding maximum query time\n".format(len(self.long_query_pids))
+
+    def process_running_report(self):
+        return "{} processes exceeding maximum process time\n".format(len(self.long_process_pids))
+
+    def term_long_queries(self):
+        cnt = 0
+        for pid in self.long_query_pids:
+            with self._conn.cursor() as crs:
+                crs.execute("select pg_terminate_backend(%s)", (pid, ))
+                (success) = crs.fetchone()
+                if success:
+                    #print("LOG: process " + str(pid) + " termianted")
+                    cnt += 1
+        return "terminated {} processes\n".format(cnt)
+
+    def term_long_processes(self):
+        cnt = 0
+        for pid in self.long_process_pids:
+            with self._conn.cursor() as crs:
+                crs.execute("select pg_terminate_backend(%s)", (pid,))
+                (success) = crs.fetchone()
+                if success:
+                    # print("LOG: process " + str(pid) + " termianted")
+                    cnt += 1
+        return "terminated {} processes\n".format(cnt)
+
+    def checkpoint(self):
+        with self._conn.cursor() as crs:
+            crs.execute("checkpoint")
+        return "checkpoint created\n"
